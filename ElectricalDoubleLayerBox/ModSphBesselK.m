898bfd148d4135e2390bfba1469479649b11c33b
sxs <song@dem.t.u-tokyo.ac.jp>
2017-02-17 21:08:46 +0900

    interaction free energy of linear PB

--- ElectricalDoubleLayerBox/ModSphBesselK.m
+++ ElectricalDoubleLayerBox/ModSphBesselK.m
@@ -1,6 +1,7 @@
 function [val] = ModSphBesselK(nu,x)
 
-val = sqrt((2/pi)./x) .* besselk(nu+0.5,x);
+% val = sqrt((2/pi)./x) .* besselk(nu+0.5,x);
+val = sqrt((pi/2)./x) .* besselk(nu+0.5,x);
 
 return
 end
